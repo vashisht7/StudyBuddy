a58c2a564c334242ec8c9d0ef798ac274a8f7096
vashisht7 <devasanivashisht@gmail.com>
2026-06-02 13:56:02 -0400

    chore: point Firebase configuration and context to newagent-5837a project

--- src/firebase.js
+++ src/firebase.js
@@ -17,13 +17,13 @@ import {
 
 // Firebase configuration retrieved from apps:sdkconfig
 const firebaseConfig = {
-  apiKey: "AIzaSyARr38GKvIzB55hbLgYliu78nTDjnRm99s",
-  authDomain: "smartmirror-ed01c.firebaseapp.com",
-  projectId: "smartmirror-ed01c",
-  storageBucket: "smartmirror-ed01c.firebasestorage.app",
-  messagingSenderId: "817403114465",
-  appId: "1:817403114465:web:f62d22ae89936e247118a0",
-  databaseURL: "https://smartmirror-ed01c.firebaseio.com"
+  apiKey: "AIzaSyD7AcN-LpyjRprGflGc8dJt0H3zOvfCgmU",
+  authDomain: "newagent-5837a.firebaseapp.com",
+  projectId: "newagent-5837a",
+  storageBucket: "newagent-5837a.firebasestorage.app",
+  messagingSenderId: "831753432864",
+  appId: "1:831753432864:web:e9cb25ca07d517fcd03145",
+  databaseURL: "https://newagent-5837a.firebaseio.com"
 };
 
 // Initialize Firebase
